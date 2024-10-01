1a44ec50f4d70c5077e0f7a0fd3d7a97d091bad8
Lucas Sanabria <sanabrialucas97@gmail.com>
2024-10-01 20:05:58 -0300

    added some styles and template management

--- secretmoviesanta/settings.py
+++ secretmoviesanta/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'secretmoviesanta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +67,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'secretmoviesanta.wsgi.application'
 
