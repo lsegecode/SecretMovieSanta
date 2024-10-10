985c9ba5d043f4f03005f8fbd0349557836cd1c3
Lucas Sanabria <sanabrialucas97@gmail.com>
2024-10-09 21:26:11 -0300

    making migrations

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Movie
 
+# Create your views here.
 def movie_list(request):
+    # Query all movies from the Movie model
     movies = Movie.objects.all()
-    return render(request, 'movie_list.html', {'movies': movies})
+
+    # Pass the movies to the 'movies_list.html' template
+    return render(request, 'movies_list.html', {'movies': movies})
